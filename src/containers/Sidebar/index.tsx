42f93669862e87e2b26f48a4578de08e1644f2f3
Armando de Caires Pinheiro <armando.caires@tecnogroup.com.br>
2024-08-24 11:20:13 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/armandodcp.png" />
-    <Titulo fontSize={18}>Armando</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={18}>Armando</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        armandodcp
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
